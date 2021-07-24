94e8fdd8d6e809be5642866dc361081fbc41219c
BHT-TK-001 <bhavinpatil56@gmail.com>
2021-07-24 11:31:34 +0530

    commit

--- main/views.py
+++ main/views.py
@@ -3,5 +3,5 @@ from django.shortcuts import render,HttpResponse
 # Create your views here.
 
 def index(request):
-    # return render(request,'index.html')
-    return HttpResponse("This is Home page")
+    return render(request,'index.html')
+    # return HttpResponse("This is Home page")
