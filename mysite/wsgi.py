a1a4eeed072f28c53bc76e928b3aaf56cb80dad2
BHT-TK-001 <bhavinpatil56@gmail.com>
2021-07-28 14:58:55 +0530

    commit

--- mysite/wsgi.py
+++ mysite/wsgi.py
@@ -7,10 +7,13 @@ For more information on this file, see
 https://docs.djangoproject.com/en/2.1/howto/deployment/wsgi/
 """
 
-import os
+import os, sys
 
 from django.core.wsgi import get_wsgi_application
 
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'mysite.settings')
 
 application = get_wsgi_application()
+
+
+application = get_wsgi_application(application)
